8bced895dabc4a5829edf6ba0aebccb4eb9fd293
QuantumK <quantumk9@hotmail.com>
2023-02-03 19:51:34 +0000

    add react router and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import ProjectGallery from "./components/ProjectGallery";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="projects" element={<ProjectGallery />} />
+
+        <Route path="contact/*" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
