3f0433691d366b8518b5ca258548ad54e2a6bc6d
QuantumK <quantumk9@hotmail.com>
2023-02-14 14:10:48 +0200

    add footer component to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
+import Footer from "../components/Footer";
 
 export default function About() {
   return (
     <div>
       <Hero />
       <Skills />
+      <Footer />
     </div>
   );
 }
