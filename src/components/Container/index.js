b36d4b63021b9c26f908cebfe84c28823b853255
QuantumK <quantumk9@hotmail.com>
2023-02-14 15:04:02 +0200

    create project and container component and their styles

---
+++ src/components/Container/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+export default function index() {
+  return (
+    <div className="container-fluid work">
+      <div className="row">
+        <div className="left-section col-xs-12 col-sm-6"></div>
+      </div>
+      <div className="right-section col-xs-12 col-sm-6 card-group"></div>
+    </div>
+  );
+}
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import ProjectGallery from "../components/ProjectGallery";
+import Hero from "../components/Hero";
 
 export default function Work() {
-  return <div>My work</div>;
+  return (
+    <div>
+      <Hero />
+      <ProjectGallery />
+    </div>
+  );
 }
