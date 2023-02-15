4de10be0a84e0769684609129f41615b84396d5e
QuantumK <quantumk9@hotmail.com>
2023-02-15 14:31:21 +0200

    create WorkHeader component and style it for Work page

--- src/components/ProjectGallery/index.js
+++ src/components/ProjectGallery/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Project from "../Project";
-import Container from "../Container";
+import WorkHeader from "../WorkHeader";
 import "./index.css";
 
 export default function index() {
   return (
     <div className="container-fluid work">
+      <div className="row">
+        <WorkHeader />
+      </div>
       <div className="row">
         <Project />
       </div>
