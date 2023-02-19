18e4eee573f79354d1bf2b58aa0bf068baa55cca
QuantumK <quantumk9@hotmail.com>
2023-02-19 15:56:40 +0000

    remove margin top and padding top from footer and remove duplicate footer from contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ContactForm from "../components/ContactForm/ContactForm.js";
 import ContactHeader from "../components/ContactHeader/ContactHeader.jsx";
-import Footer from "../components/Footer/Footer";
 
 export default function Contact() {
   return (
     <div>
       <ContactHeader />
       <ContactForm />
-      <Footer />
     </div>
   );
 }
