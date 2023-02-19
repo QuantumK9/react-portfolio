66e0123aac92ea8fddb574f232bbeff8c5582c7d
QuantumK <quantumk9@hotmail.com>
2023-02-19 14:47:37 +0000

    add ContactHeader to Contact page with styles

--- src/components/ContactHeader/ContactHeader.jsx
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ContactHeader.css";
 
 export default function ContactHeader() {
   return (
@@ -8,21 +9,14 @@ export default function ContactHeader() {
       <p class="contact-paragraph ">Go ahead... Don't be shy</p>
 
       <div class="social-wrapper">
-        <a
-          href="tel:+97254-905-0351"
-          class="image-wrapper social-links w-inline-block"
-        >
+        <a href="tel:+97254-905-0351" class="social-link-wrapper ">
           <img
             src="https://uploads-ssl.webflow.com/5f4bcd5217865e26e2333f3d/5f4f458afe5898a6ab9b58b8_Circle%20Button.svg"
             loading="lazy"
             alt="Social icon - Call"
           />
         </a>
-        <a
-          href="#"
-          target="_blank"
-          class="image-wrapper social-links w-inline-block"
-        >
+        <a href="#" target="_blank" class="social-link-wrapper">
           <img
             src="https://uploads-ssl.webflow.com/5f4bcd5217865e26e2333f3d/5f4f471c34c9ef7c52cab8f7_Circle%20Button%20Inst.svg"
             loading="lazy"
@@ -31,7 +25,7 @@ export default function ContactHeader() {
         </a>
         <a
           href="https://www.linkedin.com/in/edgar-deiner-73a2591bb"
-          class="image-wrapper social-links w-inline-block"
+          class="social-link-wrapper"
         >
           <img
             src="https://uploads-ssl.webflow.com/5f4bcd5217865e26e2333f3d/5f4f471cbe6a68b28bb19abb_Circle%20Button%20In.svg"
@@ -39,10 +33,7 @@ export default function ContactHeader() {
             alt=""
           />
         </a>
-        <a
-          href="mailto:deiner.edgar@gmail.com"
-          class="image-wrapper social-links w-inline-block"
-        >
+        <a href="mailto:deiner.edgar@gmail.com" class="social-link-wrapper">
           <img
             src="https://uploads-ssl.webflow.com/5f4bcd5217865e26e2333f3d/5f4f471c59f25d5bfd08ba05_Circle%20Button%20Mail.svg"
             loading="lazy"
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactForm from "../components/ContactForm/ContactForm.js";
+import ContactHeader from "../components/ContactHeader/ContactHeader.jsx";
 
 export default function Contact() {
   return (
     <div>
+      <ContactHeader />
       <ContactForm />
     </div>
   );
