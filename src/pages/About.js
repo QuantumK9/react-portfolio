67e9194e8e8215f3095d1e76d729391442c67c53
QuantumK <quantumk9@hotmail.com>
2023-02-19 15:02:43 +0000

    add Footer component to App and remove it from other pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
-import Footer from "../components/Footer/Footer";
 
 export default function About() {
   return (
     <div>
       <Hero />
       <Skills />
-      <Footer />
     </div>
   );
 }
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import ProjectGallery from "../components/ProjectGallery";
-import Hero from "../components/Hero";
-import Container from "../components/Container";
 
 export default function Work() {
   return (
