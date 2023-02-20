b5ed2ca36b843d7f095dbb3a2e6fabf06f6f3949
QuantumK <quantumk9@hotmail.com>
2023-02-20 20:17:49 +0000

    add card to right jumbo section and style it

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -26,13 +26,23 @@ export default function index() {
           </div>
           <div className="right-jumbo col-xs-12 col-sm-6">
             <div className="row row-col-1">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
+              <div className="card right-jumbo-card">
+                <div className="card-body">
+                  <h1 className="card-title">Card title</h1>
+                  <h2 className="card-subtitle mb-2 text-muted">
+                    Card subtitle
+                  </h2>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="card-link">
+                    Card link
+                  </a>
+                  <a href="#" className="card-link">
+                    Another link
+                  </a>
+                </div>
               </div>
             </div>
           </div>
