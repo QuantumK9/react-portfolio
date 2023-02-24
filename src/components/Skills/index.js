3ba767b0356e057e492c68c7264e0a0b999e4940
QuantumK <quantumk9@hotmail.com>
2023-02-24 20:37:00 +0000

    add images in cards and some styling

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./index.css";
+import bootstrap from "../../assets/bootstrap.jpeg";
+import mongodb from "../../assets/mongodb.png";
+import next from "../../assets/next.png";
+import node from "../../assets/node.png";
+import react from "../../assets/react.png";
+import redux from "../../assets/redux.png";
+import express from "../../assets/express.png";
 
 export default function index() {
   return (
@@ -20,54 +28,47 @@ export default function index() {
           </div>
         </div>
         <div className="right-section col-xs-12 col-sm-6 card-group">
-          <div className="row row-cols-1 row-cols-md-2">
+          <div className="row skills-card-row">
             {/* <div className="card-group"> */}
-            <div className="col mb-4">
-              <div className="card">
-                <img
-                  src="./images/02-portfolio-4.jpg"
-                  alt="an surfing in ocean"
-                  className="card-img-top"
-                />
+            <div className="col-sm-12 col-md-4 mb-1">
+              <div className="card skills-card">
+                <div className="img-wrapper">
+                  <img
+                    src={react}
+                    alt="an surfing in ocean"
+                    className="card-img-top"
+                  />
+                </div>
                 <div className="card-body">
                   <h5 className="card-title">Surf Report</h5>
                   <p className="card-text">Vanilla JS</p>
                 </div>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img
-                  src="./images/02-portfolio-4.jpg"
-                  alt="an surfing in ocean"
-                  className="card-img-top"
-                />
+            <div className="col-sm-12 col-md-4 mb-1">
+              <div className="card skills-card">
+                <div className="img-wrapper">
+                  <img
+                    src={react}
+                    alt="an surfing in ocean"
+                    className="card-img-top"
+                  />
+                </div>
                 <div className="card-body">
                   <h5 className="card-title">Surf Report</h5>
                   <p className="card-text">Vanilla JS</p>
                 </div>
               </div>
             </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img
-                  src="./images/02-portfolio-4.jpg"
-                  alt="an surfing in ocean"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Surf Report</h5>
-                  <p className="card-text">Vanilla JS</p>
+            <div className="col-sm-12 col-md-4 mb-1">
+              <div className="card skills-card">
+                <div className="img-wrapper">
+                  <img
+                    src={react}
+                    alt="an surfing in ocean"
+                    className="card-img-top"
+                  />
                 </div>
-              </div>
-            </div>
-            <div className="col mb-4">
-              <div className="card">
-                <img
-                  src="./images/02-portfolio-4.jpg"
-                  alt="an surfing in ocean"
-                  className="card-img-top"
-                />
                 <div className="card-body">
                   <h5 className="card-title">Surf Report</h5>
                   <p className="card-text">Vanilla JS</p>
@@ -75,12 +76,57 @@ export default function index() {
               </div>
             </div>
           </div>
-          <div className="row row-cols-12">
-            {/* <button> */}
-            <NavLink to="/projects" role="button" className="col btn btn-link">
+          <div className="row">
+            <div className="col-sm-12 col-md-4 mb-1">
+              <div className="card skills-card">
+                <div className="img-wrapper">
+                  <img
+                    src={react}
+                    alt="an surfing in ocean"
+                    className="card-img-top"
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">Surf Report</h5>
+                  <p className="card-text">Vanilla JS</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-md-4 mb-1">
+              <div className="card skills-card">
+                <div className="img-wrapper">
+                  <img
+                    src={react}
+                    alt="an surfing in ocean"
+                    className="card-img-top"
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">Surf Report</h5>
+                  <p className="card-text">Vanilla JS</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-12 col-md-4 mb-1">
+              <div className="card skills-card">
+                <div className="img-wrapper">
+                  <img
+                    src={react}
+                    alt="an surfing in ocean"
+                    className="card-img-top"
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">Surf Report</h5>
+                  <p className="card-text">Vanilla JS</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row r">
+            <NavLink to="/work" role="button" className=" btn btn-primary">
               Gallery
             </NavLink>
-            {/* </button> */}
           </div>
         </div>
       </div>
